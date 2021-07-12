11f0ddd8f254210ef513fc49bf98f5f38a0569ec
aafreen-15 <83712329+aafreen-15@users.noreply.github.com>
2021-07-12 13:00:16 +0800

    created css and js file

---
+++ index.js
@@ -0,0 +1,78 @@
+//time and day
+let now = new Date();
+let date = now.getDate();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let today = document.querySelector("#today-date");
+today.innerHTML = `${day} ${hour}:${minutes}`;
+//search CIty
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchCity");
+  searchInput.value = searchInput.value.toUpperCase();
+  searchInput.value = searchInput.value.trim();
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+  searchCity(searchInput.value);
+}
+function searchCity(city) {
+  let apiKey = "853a11ba7c5104766d458d020002f62f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", search);
+//converting temperature
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+//weather data
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  let humidity = response.data.main.humidity;
+  let currentHumidity = document.querySelector("#humidity");
+  currentHumidity.innerHTML = `Humidity: ${humidity}%`;
+  let wind = Math.round(response.data.wind.speed);
+  let currentWind = document.querySelector("#wind");
+  currentWind.innerHTML = `Wind: ${wind}km/h`;
+  document.querySelector("#condition").innerHTML =
+    response.data.weather[0].main;
+}
+function searchLocation(position) {
+  let apiKey = "853a11ba7c5104766d458d020002f62f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let changeCity = document.querySelector("#searchBar");
+changeCity.addEventListener("submit", search);
+let currentLocationButton = document.querySelector("#currentLocation");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("Singapore");
